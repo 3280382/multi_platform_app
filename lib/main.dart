6010164d5a44b963cfcb17d6c45808e780e7dbcc
3280382 <3280382@qq.com>
2026-03-12 17:51:16 +0800

    🔧 Fix build errors
    - Remove responsive_framework dependency (compatibility issues)
    - Remove flutter_staggered_grid_view dependency
    - Remove assets/images configuration (directory not exist)
    - Remove fonts configuration (files not exist)
    - Update dependencies to latest stable versions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 import 'app_router.dart';
 
 void main() {
@@ -40,15 +39,6 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
         useMaterial3: true,
-      ),
-      builder: (context, child) => ResponsiveBreakpoints.builder(
-        child: child!,
-        breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
-          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-        ],
       ),
       routerConfig: appRouter,
     );
